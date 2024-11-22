e2c7a252d204f1973eb82424b51b95b963a29471
devonreing <93689325+devonreing@users.noreply.github.com>
2024-11-22 16:10:22 -0500

    Create unemployment_queries.js

---
+++ unemployment_queries.js
@@ -0,0 +1,72 @@
+/* Question 1
+Query to find the number of unique years
+*/
+
+db.unemployment.aggregate([
+  {
+    $group: {
+      _id: "$Year"
+    }
+  },
+  {
+    $count: "unique_years_count"
+  }
+])
+
+/* Question 2
+Query to find the number of states in the data set
+*/
+db.unemployment.aggregate([
+  {
+    $group:
+      /**
+       * query: The query in MQL.
+       */
+      {
+        _id: "$State"
+      }
+  },
+  {
+    $count:
+      /**
+       * Provide the field name for the count.
+       */
+      "Number_of_States"
+  }
+])
+
+// Question 3
+
+db.unemployment.find({Rate : {$lt: 1.0}}).count()
+
+// This query finds the number of counties with a yearly unemployment rate < 1.0% (could have multiple years that count each time for the same county)
+
+/* Question 4
+  Query to find all counties with unemployment rate higher than 10%
+  */
+
+
+/*
+Question 5
+Query to calculate the average unemployment rate across all states.
+  */
+
+/*
+Question 6
+Query to find all counties with an unemployment rate between 5% and 8%.
+  */
+
+/*
+Question 7
+Query to find the state with the highest unemployment rate. Hint. Use { $limit: 1 }
+  */
+
+/*
+Question 8
+Query to count how many counties have an unemployment rate above 5%.
+  */
+
+/*
+Question 9
+Query to calculate the average unemployment rate per state by year.
+  */
